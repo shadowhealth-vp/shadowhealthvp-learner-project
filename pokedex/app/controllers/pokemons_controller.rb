5836287816df04c4501c64befeb7e91c55c3a902
shadowhealth-vp <phiathepv@science.regn.net>
2025-03-24 14:52:37 -0400

    Fixed errors from syntax

--- pokedex/app/controllers/pokemons_controller.rb
+++ pokedex/app/controllers/pokemons_controller.rb
@@ -3,13 +3,13 @@ require "json"
 
 class PokemonsController < ApplicationController
   def index
-    @pokemons = PokemonService.new.get_all
+    @pokemons = PokemonService.get_all
   end
 
   def show
     pokemon_id = params[:id]
 
-    @pokemon = PokemonService.new.get_by_id(pokemon_id)
+    @pokemon = PokemonService.get_by_id(pokemon_id)
 
     render :show
   end
