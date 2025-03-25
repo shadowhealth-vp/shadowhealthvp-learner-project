92f946c60aafa8226a7b19a11bc299509b085bb7
shadowhealth-vp <phiathepv@science.regn.net>
2025-03-25 08:50:12 -0400

    Fixed page refresh caused by turbo

--- pokedex/app/helpers/pokemons_helper.rb
+++ pokedex/app/helpers/pokemons_helper.rb
@@ -2,4 +2,8 @@ module PokemonsHelper
   def get_sprite_url_from_id(pokemon_id)
     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/#{pokemon_id}.png"
   end
+
+  def get_sprite_url_from_name(pokemon_name)
+    "nil"
+  end
 end
